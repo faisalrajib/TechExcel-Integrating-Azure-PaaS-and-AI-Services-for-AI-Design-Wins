34c613a52a18c0b652222ab0747c5d785e75e9cf
faisalrajib <faisal.rajib@gmail.com>
2024-11-06 13:46:06 -0500

    dd

--- src/ContosoSuitesWebAPI/Program.cs
+++ src/ContosoSuitesWebAPI/Program.cs
@@ -64,11 +64,11 @@ builder.Services.AddSingleton<AzureOpenAIClient>((_) =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
